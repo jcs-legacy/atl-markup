ab84fb1439aba2b81fd4b28e1d835bce25c53708
jenchieh <jcs090218@gmail.com>
2020-07-22 17:16:50 +0800

    Add core.

---
+++ auto-truncate-lines.el
@@ -0,0 +1,38 @@
+;;; auto-truncate-lines.el ---  Automatically truncate lines by position  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-07-22 17:11:15
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Automatically truncate lines by position.
+;; Keyword: automatic truncate visual lines
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/auto-truncate-lines
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Automatically truncate lines by position.
+;;
+
+;;; Code:
+
+
+
+(provide 'auto-truncate-lines)
+;;; auto-truncate-lines.el ends here
